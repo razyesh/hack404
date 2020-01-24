e093b81dfc4e128a766ec668cb55afbcf3ad6a1f
utshaav025 <utshaav583@gmail.com>
2020-01-24 22:30:25 +0545

    nothing

--- product/models.py
+++ product/models.py
@@ -8,9 +8,8 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length= 20)
     item_type = models.ForeignKey(Category,related_name="Product", on_delete=models.PROTECT)
-    image = models.ImageField(upload_to="/product/media")
+    image = models.ImageField(upload_to="product/media")
     price = models.FloatField()
     description = models.TextField(max_length=200)
     choice = models.BooleanField(default=False)
-
 
